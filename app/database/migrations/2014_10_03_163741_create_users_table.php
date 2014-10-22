bbda8fc5920ad6eb22c2d7a06654d9a57409fe7c
Lamberto Guevarra <seoblogger07@gmail.com>
2014-10-22 13:37:11 +0800

    set database seeder

--- app/database/migrations/2014_10_03_163741_create_users_table.php
+++ app/database/migrations/2014_10_03_163741_create_users_table.php
@@ -18,30 +18,6 @@ class CreateUsersTable extends Migration {
 			$table->string('password');
 			$table->timestamps();
 		});
-
-		DB::table('users')->insert(
-			array(
-				array( 
-						'username' => 'admin',
-						'password' => Hash::make('admin'),
-						'created_at' => date('Y-m-d H:i:s'),
-						'updated_at' => date('Y-m-d H:i:s')
-					),
-				array( 
-						'username' => 'angel',
-						'password' => Hash::make('angel123'),
-						'created_at' => date('Y-m-d H:i:s'),
-						'updated_at' => date('Y-m-d H:i:s')
-					),
-				array( 
-						'username' => 'van',
-						'password' => Hash::make('van123'),
-						'created_at' => date('Y-m-d H:i:s'),
-						'updated_at' => date('Y-m-d H:i:s')
-					)
-			)
-		);
-
 	}
 
 	/**
