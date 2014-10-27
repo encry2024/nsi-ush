1367d3e7df6b7d70a11d694f45d1845c9c54598d
Lamberto Guevarra <seoblogger07@gmail.com>
2014-10-27 12:46:17 +0800

    update sidebar

--- app/views/sidebar/supervisor.blade.php
+++ app/views/sidebar/supervisor.blade.php
@@ -5,12 +5,15 @@
 <div class="row collapse">
 <div class="large-12 columns">
 	<ul class="side-nav">
-		<li>{{ HTML::link("/", "Fetch Sales") }}</li>
-		<li>{{ HTML::link("unsubmitted-sales", "Unsubmitted (" . $summary['unsubmitted'] . ")") }}</li>
-		<li>{{ HTML::link("unsuccessful-sales", "Unsuccessful (" . $summary['unsuccessful'] . ")") }}</li>
-		<li>{{ HTML::link("successful-sales", "Successful (" . $summary['successful'] . ")") }}</li>
-		<li>{{ HTML::link("cancel-sales", "Cancelled (" . $summary['cancel'] . ")") }}</li>
-		<li>{{ HTML::link("reports", "Reports") }}</li>
+		<li class="heading">{{ HTML::link("/", "Fetch Sales") }}</li>
+		<li class="heading">{{ HTML::link("lead/add", "Add Lead") }}</li>
+		<li class="heading">{{ HTML::link("reports", "Reports") }}</li>
+		<li class="divider"></li>
+		<li class="heading">Lead Summary</li>
+		<li>{{ HTML::link("unsubmitted-sales", " - Unsubmitted (" . $summary['unsubmitted'] . ")") }}</li>
+		<li>{{ HTML::link("unsuccessful-sales", " - Unsuccessful (" . $summary['unsuccessful'] . ")") }}</li>
+		<li>{{ HTML::link("successful-sales", " - Successful (" . $summary['successful'] . ")") }}</li>
+		<li>{{ HTML::link("cancel-sales", " - Cancelled (" . $summary['cancel'] . ")") }}</li>
 	</ul>
 </div>
 </div>
