afa8cf7799f78c75f2b285c7545c753214c473d4
Lamberto Guevarra <seoblogger07@gmail.com>
2014-11-10 11:49:09 +0800

    update table views

--- app/views/sale_submission.blade.php
+++ app/views/sale_submission.blade.php
@@ -16,7 +16,8 @@
 						<th class="large-2">Name</th>
 						<th class="large-2">Work Phone</th>
 						<th class="large-2">State</th>
-						<th class="large-2">Entry Date</th>
+						<th class="large-2">Sale Date(EST)</th>
+						<th class="large-2">Entry date</th>
 					</tr>
 				</thead>
 				<tbody>
@@ -27,6 +28,7 @@
 						<td><a href="{{ URL::to('lead/'.$sale->id); }}">{{ $sale->getName() }}</a></td>
 						<td>{{ $sale->workphone }}</td>
 						<td>{{ $sale->state }}</td>
+						<td>{{ date('M j, Y', strtotime($sale->vici_sale_date)) }}</td>
 						<td>{{ date('M j, Y', strtotime($sale->created_at)) }}</td>
 					</tr>
 					@endforeach
